a7b68914eef6c76ace586347eb5bf9320bf0497e
BENSON <bensondaniel664@gmail.com>
2017-05-11 16:49:03 -0500

    change

--- modules/webhookConekta/controllers/webhook.php
+++ modules/webhookConekta/controllers/webhook.php
@@ -10,17 +10,21 @@ class webhook extends oxUBase {
     http_response_code(200); // Return 200 OK
 
     if ($data->type == 'charge.paid'){
+      $payment_method = $data->charges->data->object->payment_method->type;
       $msg = "Tu pago ha sido comprobado.";
-      $subject = "Confirmación de Pago OXXO Green Peach Super Foods";
-      $to = "bensondaniel664@gmail.com";
-      $nombre = "Daniel";
-      $email = "bensondaniel664@gmail.com";
-      $oEmail = oxNew('oxEmail');
-      $oEmail->setBody($msg);
-      $oEmail->setSubject($subject);
-      $oEmail->setRecipient($to,'Benson');
-      $oEmail->setReplyTo($email, $nombre);
-      $oEmail->send();
+      mail("client@email.com","Pago ". $payment_method ." confirmado",$msg);
+      // $msg = "Tu pago ha sido comprobado.";
+      // $subject = "Confirmación de Pago OXXO Green Peach Super Foods";
+      // $to = "bensondaniel664@gmail.com";
+      // $nombre = "Daniel";
+      // $email = "bensondaniel664@gmail.com";
+      // $oEmail = oxNew('oxEmail');
+      // $oEmail->setBody($msg);
+      // $oEmail->setSubject($subject);
+      // $oEmail->setRecipient($to,'Benson');
+      // $oEmail->setReplyTo($email, $nombre);
+      // $oEmail->send();
+
     }
   }
 
