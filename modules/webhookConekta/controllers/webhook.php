e8c2b94bcb02944d0091f85aab6db7aae692734f
BENSON <bensondaniel664@gmail.com>
2017-05-11 14:26:58 -0500

    change

--- modules/webhookConekta/controllers/webhook.php
+++ modules/webhookConekta/controllers/webhook.php
@@ -7,11 +7,12 @@ class webhook extends oxUBase {
 
     $body = @file_get_contents('php://input');
     $data = json_decode($body);
-    http_response_code(422); // Return 200 OK
+    http_response_code(200); // Return 200 OK
 
     if ($data->type == 'charge.paid'){
-      $msg = "Tu pago ha sido comprobado.";
-      mail("bensondaniel664@gmail.com","Pago confirmado",$msg);
+      // $msg = "Tu pago ha sido comprobado.";
+      // mail("bensondaniel664@gmail.com","Pago confirmado",$msg);
+      echo "Pagado";
     }
   }
 
